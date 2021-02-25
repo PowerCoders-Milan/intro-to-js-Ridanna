4a58a020abb72bb373ae8b094ce9d41b89ffeb69
mahmudref@gmail.com <mahmudref@gmail.com>
2021-02-25 12:23:48 +0100

    exercise1

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,2 +1,8 @@
 // Example
-window.alert("Hello World!");
+var numberOfChildren = 3;
+var partnerName = 'Nicole';
+var geographicalLocation = 'Milan';
+var jobTitle = 'Devops Engineer'
+var fortureTeller = 'you will be a ' + jobTitle + ' in ' + geographicalLocation + ' , and married to ' + partnerName + ' ' + ' with ' + numberOfChildren + ' kids.';
+console.log(fortureTeller);
+window.alert(fortureTeller);
