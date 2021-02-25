7260e8a20634887aa70249d9e7cb6f83333ae14a
mahmudref@gmail.com <mahmudref@gmail.com>
2021-02-25 14:43:41 +0100

    exercise 4

--- ex-4/exercise.js
+++ ex-4/exercise.js
@@ -1,2 +1,12 @@
 // Example
-window.alert("Hello World!");
+
+var name = prompt("Please enter your name:");
+window.alert('Hello ' + name)
+var days = prompt("How was your day?")
+
+if (days == 'good') {
+    window.alert('I am happy for you champ')
+}
+if (days == 'bad') {
+    window.alert('That is too bad, tomorrow it is a new day');
+}
