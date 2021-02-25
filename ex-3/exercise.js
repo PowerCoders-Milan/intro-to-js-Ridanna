84da0fbc5e5fed7e94236532e6d36092be986da9
mahmudref@gmail.com <mahmudref@gmail.com>
2021-02-25 12:53:47 +0100

    exercise3

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,2 +1,12 @@
 // Example
-window.alert("Hello World!");
+var number = 8;
+var numberTwo = 10;
+
+if (number > numberTwo) {
+    console.log("The greater number of " + number + " and " + numberTwo + " is " + number + ".")
+} else if (numberTwo > number) {
+    console.log("The greater number of " + number + " and " + numberTwo + " is " + numberTwo + ".") 
+} else {
+    console.log('the numbers are the same')
+}
+
